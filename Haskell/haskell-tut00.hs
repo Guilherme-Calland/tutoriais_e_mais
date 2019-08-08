327d1edb26c6a6d0250c4c6b99bcba6d861e5983
Guilherme Calland Rosa Borba <gcrb@WINDOWS.CIN.UFPE.BR>
2019-08-08 14:58:20 -0300

    14:30 no video de haskell

---
+++ Haskell/haskell-tut00.hs
@@ -0,0 +1,20 @@
+import Data.List
+import System.IO
+ 
+-- comment
+ {-
+ multi-line comment
+-}
+
+x = 50
+somaDeDez = sum[1..10]
+modEx = mod 10 4
+y = 5 - 4
+trueAndFalse = True && False
+trueOrFalse = True || False
+notTrue = not(True)
+listaDeNums = [10,12,20,3,50,101]
+outraLista = 5 : 10 : 12 : 7 : []
+multList = [[1,2,3],[4,5,6]]
+maisNums = 202 : listaDeNums
+lengList = length maisNums
